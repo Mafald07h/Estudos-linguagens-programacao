6e40e659b4e2b8a096615a1eef7c0522afeb3f78
Heitor <mafaldoheitor@gmail.com>
2026-01-31 09:41:37 -0300

    docs: Adicionando conceito de variáveis locais

---
+++ linguagem-c/local-global.c
@@ -0,0 +1,17 @@
+// Variáveis locais e globais
+
+/* Variáveis locais existem em um bloco de código específico, sendo 
+delimitado por chaves { }, além disso, essas variáveis são criadas quando se chama uma função
+e destruidas ao se encerrar a função.*/
+#include <stdio.h>
+/* Nesse exemplo, as variáveis 'a' e 'b' só existem nessa função 'soma', e 
+são criadas quando se chama essa função soma para algum bloco de código */
+int soma(int a,int b){
+        return a+b;
+    }
+
+int main(){
+    int contador = 1;
+    printf("A soma é: %d \n",soma(8,7));
+    return 0;
+}
