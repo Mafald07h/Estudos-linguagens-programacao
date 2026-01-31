c81801324b0ad2370fabaec9d97ba5a0e7412741
Heitor <mafaldoheitor@gmail.com>
2026-01-31 16:00:15 -0300

    feat: Criando o arquivo de repetição em Java

---
+++ linguagem-java/Repeticao.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Repeticao {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        List<Integer> numerosInteervalo = new ArrayList<>();
+
+        System.out.println("Informe o início do intervalo: ");
+        int inicio = entrada.nextInt();
+        System.out.println("Informe o fim do intervalo: ");
+        int fim = entrada.nextInt();
+        for (int i = inicio+1; i<fim;i++){
+            numerosInteervalo.add(i);
+        }
+        System.out.println("Os números presentes no intervalo é: " + numerosInteervalo);
+    }
+}
