36f5ec6cc25d9efa9cf3aceffe450ad86989d83a
Heitor <mafaldoheitor@gmail.com>
2026-01-31 10:49:06 -0300

    feat: Adicionando arquivo de entrada de dados

---
+++ linguagem-cplusplus/entrada-dados.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int numero1,numero2,soma;
+    cout << "Informe o primeiro número: " << endl;
+    cin >> numero1;
+    cout << "Informe o segundo número: " << endl;
+    cin >> numero2;
+    soma = numero1+numero2;
+    cout << "A soma é: " << soma << endl;
+    return 0;
+}
