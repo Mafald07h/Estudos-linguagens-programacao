a71eec730bb6a308f907133e7ffef7a7dd623277
Heitor <mafaldoheitor@gmail.com>
2026-01-31 10:08:33 -0300

    feat: Adicionando o arquivo executável de ponto flutuante

--- linguagem-c/pontos-flutuantes.c
+++ linguagem-c/pontos-flutuantes.c
@@ -7,6 +7,6 @@ int main(){
     float variavel2 = 1.0F;
     // 'unsigned' se utiliza o U
     unsigned int variavel3 = 20U;
-    printf("Três variáveis: %d%d%d",variavel1,variavel2,variavel3);
+    printf("Três variáveis: %ld %f %d \n",variavel1,variavel2,variavel3);
     return 0;
 }
