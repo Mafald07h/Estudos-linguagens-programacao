8015b1f9312a0bbb6e11f5760993d8a56a2a7d2a
Heitor <mafaldoheitor@gmail.com>
2026-01-31 10:06:08 -0300

    feat: Adicionando arquivo de estudo de pontos flutuantes

---
+++ linguagem-c/pontos-flutuantes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// Números de ponto flutuante
+int main(){
+    // 'long int se utiliza o L'
+    long int variavel1 = 5000L;
+    // 'Ponto flutuante' utiliza o F
+    float variavel2 = 1.0F;
+    // 'unsigned' se utiliza o U
+    unsigned int variavel3 = 20U;
+    printf("Três variáveis: %d%d%d",variavel1,variavel2,variavel3);
+    return 0;
+}
