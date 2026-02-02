0eea1146e0f13da2181d90ed95680743d9dcf5ae
Heitor <mafaldoheitor@gmail.com>
2026-02-01 22:55:08 -0300

    docs: Adicionando exemplo de arrays

--- linguagem-java/Arrays.java
+++ linguagem-java/Arrays.java
@@ -1,5 +1,16 @@
+import java.util.List;
+
 public class Arrays {
     public static void main(String[] args) {
-        
+        int[] c;
+        c = new int[12]; // Total de elementos que pode ter nesse 'Array'
+
+        String[] b = new String[100],x = new String[20];
+        // Segunda forma de se criar dois arrays
+        for (int i = 0; i < c.length; i++){
+            System.out.println(c[i]);
+        }
+
+        // Todo o Array contem elementos 0
     }
 }
