8eff4415aca5a7268bb4eb278a5034c5deb33240
Heitor <mafaldoheitor@gmail.com>
2026-02-03 20:18:17 -0300

    feat: Criando o arquivo de condições em C++

---
+++ linguagem-cplusplus/condicoes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int number = 10;
+    if (number>20){
+        cout << "Number is more than 20";
+    }
+    else {
+        cout << "Number is small than 20";
+    }
+    return 0;
+}
