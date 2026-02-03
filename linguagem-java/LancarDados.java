4616579e92ab0a3d85e0018a85ed95bc5f8dadc4
Heitor <mafaldoheitor@gmail.com>
2026-02-02 21:14:44 -0300

    docs: Adicionando o game a classe LancarDados

--- linguagem-java/LancarDados.java
+++ linguagem-java/LancarDados.java
@@ -1,5 +1,18 @@
+import java.security.SecureRandom;
+
 public class LancarDados {
     public static void main(String[] args) {
-        
+        SecureRandom randomNumber = new SecureRandom();
+        int[] frequencia = new int[7];
+
+        for(int lancamento = 0; lancamento <= 50; lancamento++) {
+            ++frequencia[1+randomNumber.nextInt(6)];
+
+        }
+
+        System.out.printf("%s%10s%n","Face","Frequência");
+
+        for (int face = 1; face < frequencia.length; face++)
+        System.out.printf("%4d%10d%n", face, frequencia[face]); 
     }
 }
