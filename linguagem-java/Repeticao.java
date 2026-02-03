d33faab6367839e46432dd3d4a12b30629d4523e
Heitor <mafaldoheitor@gmail.com>
2026-02-03 08:42:26 -0300

    feat: Adicionando um novo estilo de estrutura de repetição

--- linguagem-java/Repeticao.java
+++ linguagem-java/Repeticao.java
@@ -6,21 +6,28 @@ public class Repeticao {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        List<Integer> numerosInteervalo = new ArrayList<>();
+        // List<Integer> numerosInteervalo = new ArrayList<>();
 
-        System.out.println("Informe o início do intervalo: ");
-        int inicio = entrada.nextInt();
-        System.out.println("Informe o fim do intervalo: ");
-        int fim = entrada.nextInt();
-        for (int i = inicio+1; i<fim;i++){
-            numerosInteervalo.add(i);
-        }
-        System.out.println("Os números presentes no intervalo é: " + numerosInteervalo);
+        // System.out.println("Informe o início do intervalo: ");
+        // int inicio = entrada.nextInt();
+        // System.out.println("Informe o fim do intervalo: ");
+        // int fim = entrada.nextInt();
+        // for (int i = inicio+1; i<fim;i++){
+        //     numerosInteervalo.add(i);
+        // }
+        // System.out.println("Os números presentes no intervalo é: " + numerosInteervalo);
 
-        System.out.println("Informe um valor em inteiro: ");
-        int valorInteiro = entrada.nextInt();
-        while (valorInteiro <= 20){
-            System.out.println(valorInteiro++);
+        // System.out.println("Informe um valor em inteiro: ");
+        // int valorInteiro = entrada.nextInt();
+        // while (valorInteiro <= 20){
+        //     System.out.println(valorInteiro++);
+        // }
+
+        int indiceLista = 0;
+        int[] numerosDefinidos = {10,45,25,36,75,44,63,24,95,11};
+        for(int valores : numerosDefinidos){
+            System.out.println("Valor da lista no índice " + indiceLista + ": " + valores);
+            indiceLista++;
         }
     }
 }
