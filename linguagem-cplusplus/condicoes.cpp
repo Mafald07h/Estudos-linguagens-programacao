7b769302265a75841b2a4e7b1f1259c6fe08028b
Heitor <mafaldoheitor@gmail.com>
2026-02-03 20:20:01 -0300

    feat: Criando o arquivo de inicialização do código de condições.cpp

--- linguagem-cplusplus/condicoes.cpp
+++ linguagem-cplusplus/condicoes.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main(){
     int number = 10;
     if (number>20){
-        cout << "Number is more than 20";
+        cout << "Number is more than 20" << endl;
     }
     else {
-        cout << "Number is small than 20";
+        cout << "Number is small than 20" << endl;
     }
     return 0;
 }
