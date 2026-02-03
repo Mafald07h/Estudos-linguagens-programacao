e225411320b8ee50ed3d83dd4ae37bc7a224d425
Heitor <mafaldoheitor@gmail.com>
2026-02-03 08:27:39 -0300

    feat: Criando uma classe de metodos

---
+++ linguagem-java/Metodos.java
@@ -0,0 +1,12 @@
+public class Metodos {
+    public static void main(String[] args) {
+        Metodos metodo = new Metodos();
+        System.out.println(metodo.somarNumeros(10, 02));
+    }
+
+
+    public int somarNumeros(int a, int b){
+        return a+b;
+    }
+
+}
