ae0e4843ef8c4dca7ed696799ad3480911522fb4
Heitor <mafaldoheitor@gmail.com>
2026-02-03 20:21:45 -0300

    docs: Adicionando o 'else if' em  condições.cpp

--- linguagem-cplusplus/condicoes.cpp
+++ linguagem-cplusplus/condicoes.cpp
@@ -7,6 +7,9 @@ int main(){
     if (number>20){
         cout << "Number is more than 20" << endl;
     }
+    else if(number==20){
+        cout << "The number is 20" << endl;
+    }
     else {
         cout << "Number is small than 20" << endl;
     }
