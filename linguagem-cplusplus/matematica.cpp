84c42f7052dfa2ee678560d912b161b15f939efc
Heitor <mafaldoheitor@gmail.com>
2026-02-09 18:07:04 -0300

    docs(matematica.cpp): Adicionar mais exemplos de funções da biblioteca cmath

--- linguagem-cplusplus/matematica.cpp
+++ linguagem-cplusplus/matematica.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 int main(){
-    cout << sqrt(900.0) << endl;
+    cout << sqrt(900.0) << endl; /* Raiz quadrada */
+    cout << ceil(9.2) << endl; /* Arredonda um número  */
+    cout << exp(1.0) << endl; /* Função exponencia e^x*/
     return 0;
 }
