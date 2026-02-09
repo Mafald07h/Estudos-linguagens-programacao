599263269746378291a52d693f92bf3ff432ac9c
Heitor <mafaldoheitor@gmail.com>
2026-02-09 18:11:02 -0300

    docs(matematica.cpp): Adicionar mais exemplos, como o exemplo de log10

--- linguagem-cplusplus/matematica.cpp
+++ linguagem-cplusplus/matematica.cpp
@@ -7,5 +7,6 @@ int main(){
     cout << sqrt(900.0) << endl; /* Raiz quadrada */
     cout << ceil(9.2) << endl; /* Arredonda um número  */
     cout << exp(1.0) << endl; /* Função exponencia e^x*/
+    cout << log10(100) << endl; /* Log de x na base 10 */
     return 0;
 }
