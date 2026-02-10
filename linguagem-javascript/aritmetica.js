ac61afc99279ffafd1063994e6d5453103c2ae24
Heitor <mafaldoheitor@gmail.com>
2026-02-10 12:28:57 -0300

    docs(aritmetica.js): Adicionar exemplos de operações aritméticas com a classe Math

---
+++ linguagem-javascript/aritmetica.js
@@ -0,0 +1,11 @@
+// Aritmetica em Javascript, trazendo exemplos
+
+console.log(Math.log10(100))
+console.log(Math.abs(-9))
+console.log(Math.abs(14))
+console.log(Math.min(14,1,23,4,65))
+console.log(Math.max(14,1,23,4,65))
+console.log(Math.pow(3,4)) // 0 <= x <= 1.0
+console.log(Math.random())
+console.log(Math.E)
+console.log(Math.PI)
