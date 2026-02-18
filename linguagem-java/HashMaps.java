10c0445ec6f26c66d05411e89271d368886021da
Heitor <mafaldoheitor@gmail.com>
2026-02-18 19:27:21 -0300

    Adicionar exemplos com hashmap

--- linguagem-java/HashMaps.java
+++ linguagem-java/HashMaps.java
@@ -5,5 +5,10 @@ public class HashMaps {
     public static void main(String[] args) {
         HashMap<String,Integer> primeiroDicionario = new HashMap();
         
+        primeiroDicionario.put("Heitor",18);
+        primeiroDicionario.put("Helena",10);
+        primeiroDicionario.put("Enzo",24);
+
+        System.out.println(primeiroDicionario);
     }
 }
