13dbc44892e35be02f5528bdf238d303205c260c
Heitor <mafaldoheitor@gmail.com>
2026-02-18 19:34:17 -0300

    Adicionar exemplos com hashmap

--- linguagem-java/HashMaps.java
+++ linguagem-java/HashMaps.java
@@ -8,9 +8,13 @@ public class HashMaps {
         primeiroDicionario.put("Heitor",18);
         primeiroDicionario.put("Helena",10);
         primeiroDicionario.put("Enzo",24);
+        primeiroDicionario.put("Thiago",20);
 
         System.out.println(primeiroDicionario);
         System.out.println(primeiroDicionario.get("Heitor"));
         System.out.println(primeiroDicionario.get("Enzo"));
+
+        primeiroDicionario.remove("Thiago");
+        System.out.println(primeiroDicionario);
     }
 }
