afdc487d4c129843fb2514b942509d95015d693d
Heitor <mafaldoheitor@gmail.com>
2026-02-18 19:36:54 -0300

    Criar exemplos com \t

--- linguagem-java/Index.java
+++ linguagem-java/Index.java
@@ -1,5 +1,6 @@
 public class Index{
     public static void main(String[] args) {
         System.out.println("Olá Java no VSCODE");
+        System.out.print("\t Eu estou utilizando diferentes prints em java");
     }
 }
