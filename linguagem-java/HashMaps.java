a7d85ab687201a38a805ad007ef510ce353e0f2f
Heitor <mafaldoheitor@gmail.com>
2026-02-18 19:25:35 -0300

    Criar o primeiro HashMap em Java

---
+++ linguagem-java/HashMaps.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class HashMaps {
+    public static void main(String[] args) {
+        HashMap<String,Integer> primeiroDicionario = new HashMap();
+        
+    }
+}
